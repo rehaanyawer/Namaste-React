ad969ddf902fc85abdeb6ca84df57b62eb0a0ce4
Yawar <rehaanyawer57@gmail.com>
2024-01-15 23:38:18 +0530

    Unit Testing done

--- src/components/__test__/Header.test.js
+++ src/components/__test__/Header.test.js
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Header from '../Header';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import appStore from '../../utils/appStore';
 import '@testing-library/jest-dom';
 
-it('should render Header component with a login button', () => {
+it('should render Header component with a login button using Role', () => {
   render(
     <BrowserRouter>
       <Provider store={appStore}>
@@ -19,7 +19,7 @@ it('should render Header component with a login button', () => {
   expect(loginButton).toBeInTheDocument();
 });
 
-it('should render Header component with a login button', () => {
+it('should render Header component with a login button using text', () => {
   render(
     <BrowserRouter>
       <Provider store={appStore}>
@@ -28,7 +28,53 @@ it('should render Header component with a login button', () => {
     </BrowserRouter>
   );
 
-  const loginButton = screen.getByRole('button');
+  //const loginButton = screen.getByRole('button');
+  const loginButton = screen.getByText('Login');
+  expect(loginButton).toBeInTheDocument();
+});
+
+it('should render Header component with a login button using Role with text', () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole('button', { name: 'Login' });
 
   expect(loginButton).toBeInTheDocument();
+});
+
+it('should render Header component with a login button but half text matching using regex', () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByText(/Cart/);
+
+  expect(loginButton).toBeInTheDocument();
+});
+
+it('should render Header component with a login button changing to logout (toggle)', () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole('button', { name: 'Login' });
+
+  fireEvent.click(loginButton);
+
+  const logoutButton = screen.getByText('Logout');
+
+  expect(logoutButton).toBeInTheDocument();
 });
